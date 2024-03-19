fbca9ebb6fd470bb75dbad478d313fca6ef3e1b6
JishnuTP <tpjishnu5@gmail.com>
2024-03-19 18:44:18 +0530

    last

--- App.js
+++ App.js
@@ -159,7 +159,7 @@ app.use((err, req, res, next) => {
     res.status(500).send('Something broke!');
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log("Server running...!");
